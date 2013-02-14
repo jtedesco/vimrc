fcdeb14f023060d8e5c187612d212c44bfdb2ff3
Jon Tedesco <jon@jontedesco.net>
2013-02-13 21:50:53 -0600

    Updated autoinstall script to take bash arg

--- autoinstall.sh
+++ autoinstall.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
-INSTALL_TO=~/Projects
+
+# Check that some argument was provided
+if [ $# -eq "0" ] # should check for no arguments
+then
+    echo "\n\tUsage: autoinstall.sh <INSTALL_TO_PATH>\n"
+    exit $E_OPTERROR
+fi
+
+INSTALL_TO=$1
+echo "Downloading to $1..."
 
 warn() {
     echo "$1" >&2
@@ -27,6 +36,7 @@ cd vim/ruby/command-t
 (ruby extconf.rb && make clean && make) || warn "Ruby compilation failed. Ruby not installed, maybe?"
 
 # Symlink ~/.vim and ~/.vimrc
+echo "Installing vim configuration to ~/"
 cd ~
 ln -s "$INSTALL_TO/vimrc/vimrc" .vimrc
 ln -s "$INSTALL_TO/vimrc/vim" .vim
